26daeb4f2a9c51d0de3d3747a85dd53f86ff1902
Varun-3131 <srivastavvarun48@gmail.com>
2024-02-14 22:39:45 +0530

    Palindrome String

---
+++ src/main/java/Main/PalindromeString.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class PalindromeString {
+    class Solution {
+        int isPalindrome(String S) {
+            // code here
+            int n=S.length();
+            for(int i=0;i<S.length();i++)
+            {
+                if(S.charAt(i)!=S.charAt(n-i-1))
+                    return 0;
+            }
+            return 1;
+        }
+    };
+}
