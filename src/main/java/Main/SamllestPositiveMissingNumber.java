9c7f68432182fa412d8d2b9ac8368dc0d04b7bff
Varun-3131 <srivastavvarun48@gmail.com>
2024-02-21 23:26:04 +0530

    Smallest Positive Missing Number...

---
+++ src/main/java/Main/SamllestPositiveMissingNumber.java
@@ -0,0 +1,18 @@
+package Main;
+
+public class SamllestPositiveMissingNumber {
+    static int missingNumber(int[] arr, int size) {
+        // Your code here
+        Arrays.sort(arr);
+        int missing = 1;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] < 1) continue;
+            else if (arr[i] == missing) {
+                missing++;
+
+            }
+
+        }
+        return missing;
+    }
+}
