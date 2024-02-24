d018a98dc44fc75f097057826afb3791c6a78653
Varun-3131 <srivastavvarun48@gmail.com>
2024-02-24 21:15:15 +0530

    Key Pair...

---
+++ src/main/java/Main/KeyPair.java
@@ -0,0 +1,26 @@
+package Main;
+
+public class KeyPair {
+    boolean hasArrayTwoCandidates(int arr[], int n, int x) {
+        // code here
+        Arrays.sort(arr);
+
+        int lp= 0;
+        int rp = n-1;
+
+        while(lp != rp){
+
+            if(arr[lp]+arr[rp] == x){
+                return true;
+            }
+
+            if(arr[lp]+ arr[rp] < x){
+                lp++;
+            }
+            else{
+                rp--;
+            }
+        }
+        return false;
+    }
+}
