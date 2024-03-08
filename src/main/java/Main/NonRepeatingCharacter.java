e8c324befce11e2377b0f0c48e4880c4399b6182
Varun-3131 <srivastavvarun48@gmail.com>
2024-03-08 22:08:39 +0530

    Non-Repeating Character...

---
+++ src/main/java/Main/NonRepeatingCharacter.java
@@ -0,0 +1,23 @@
+package Main;
+
+public class NonRepeatingCharacter {
+    static char nonrepeatingCharacter(String S)
+    {
+        //Your code here
+
+        Map<Character,Integer> m = new HashMap();
+        int n = S.length();
+        for(int i=0;i<n;i++){
+            char ch = S.charAt(i);
+            m.put(ch,m.getOrDefault(ch,0)+1);
+        }
+        for(int i=0;i<n;i++){
+            char ch = S.charAt(i);
+            if(m.get(ch)==1){
+                return ch;
+            }
+        }
+        return '$';
+
+    }
+}
