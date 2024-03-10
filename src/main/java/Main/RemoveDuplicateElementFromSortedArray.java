08a49fb8a0d5e389b0443897bb1fd19c9d6d5c60
Varun-3131 <srivastavvarun48@gmail.com>
2024-03-10 22:26:49 +0530

    Remove Duplicate Element From Sorted Array...

---
+++ src/main/java/Main/RemoveDuplicateElementFromSortedArray.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class RemoveDuplicateElementFromSortedArray {
+    int remove_duplicate(int arr[],int N){
+        // code here
+        int i = 0;
+        int j = 1;
+        while(j < N){
+            if(arr[i] != arr[j]){
+                i++;
+                arr[i] = arr[j];
+            }
+            j++;
+        }
+        return i+1;
+    }
+}
