7ff7ef8d9a3f0efffcb89d1e1cbe3380f12e7c2a
Varun-3131 <srivastavvarun48@gmail.com>
2024-03-23 18:56:13 +0530

    Occurence Of An Integer In A Linked List...

---
+++ src/main/java/Main/OccurenceOfAnIntegerInALinkedList.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class OccurenceOfAnIntegerInALinkedList {
+    public static int count(Node head, int key)
+    {
+        //code here
+        int count = 0;
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == key)count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+}
