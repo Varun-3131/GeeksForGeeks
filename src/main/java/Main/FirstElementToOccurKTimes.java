11c4c01aaba10b25ddabb305d7e3f89de0ffb493
Varun-3131 <srivastavvarun48@gmail.com>
2024-04-05 23:13:48 +0530

    First Element To Occur K Times...

---
+++ src/main/java/Main/FirstElementToOccurKTimes.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class FirstElementToOccurKTimes {
+    public int firstElementKTime(int n, int k, int[] a) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            if(!map.containsKey(a[i])) map.put(a[i],1);
+            else map.put(a[i],map.get(a[i])+1);
+            if(map.get(a[i])>=k) return a[i];
+        }
+        return -1;
+    }
+}
