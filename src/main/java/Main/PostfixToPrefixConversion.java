a7cdb8c3dabada1461004e4681208b84a62bc77c
Varun-3131 <srivastavvarun48@gmail.com>
2024-04-22 23:01:53 +0530

    Postfix To Prefix Conversion...

---
+++ src/main/java/Main/PostfixToPrefixConversion.java
@@ -0,0 +1,18 @@
+package Main;
+
+public class PostfixToPrefixConversion {
+    static String postToPre(String post_exp) {
+        // code here
+        Stack <String> s= new Stack<>();
+        for(int i=0;i<post_exp.length();i++){
+            char c= post_exp.charAt(i);
+            if(Character.isLetter(c))s.push(String.valueOf(c));
+            else{
+                String v=s.pop();
+                s.push(c+s.pop()+v);
+
+            }
+        }
+        return s.pop();
+    }
+}
