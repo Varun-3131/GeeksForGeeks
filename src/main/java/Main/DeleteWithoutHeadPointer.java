693049f70c053282a47442d92c6d18ffa0455336
Varun-3131 <srivastavvarun48@gmail.com>
2024-04-25 01:03:59 +0530

    Delete Without Head Pointer...

---
+++ src/main/java/Main/DeleteWithoutHeadPointer.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class DeleteWithoutHeadPointer {
+    void deleteNode(Node node) {
+        //Add your code here
+        if(node.next==null){
+            return;
+        }
+        node.data=node.next.data;
+        node.next=node.next.next;
+    }
+}
