8b385efe87e192ec59f6b20bf7dd50a4175fabf4
Varun-3131 <srivastavvarun48@gmail.com>
2024-05-24 21:18:10 +0530

    Queue Reversal...

---
+++ src/main/java/Main/QueueReversal.java
@@ -0,0 +1,20 @@
+package Main;
+
+public class QueueReversal {
+    //Function to reverse the queue.
+    public Queue<Integer> rev(Queue<Integer> q){
+        //add code here.
+        ArrayDeque<Integer> s=new ArrayDeque<>();
+
+        while(!q.isEmpty()){
+            s.push(q.poll());
+        }
+
+        while(!s.isEmpty()){
+            q.offer(s.pop());
+        }
+
+
+        return q;
+    }
+}
