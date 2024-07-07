3749b21c6fa77775d3692024579e99e8f51d848f
Varun-3131 <srivastavvarun48@gmail.com>
2024-07-07 14:08:07 +0530

    nCr...

--- src/main/java/Main/nCr.java
+++ src/main/java/Main/nCr.java
@@ -1,34 +1,25 @@
 package Main;
 
 public class nCr {
-    public String reverseVowels(String s) {
-        char[] word = s.toCharArray();
-        int start = 0;
-        int end = s.length() - 1;
-        String vowels = "aeiouAEIOU";
+    static int mod = (int)1e9 + 7;
 
-        while (start < end) {
-
-            while (start < end && vowels.indexOf(word[start]) == -1) {
-                start++;
-            }
-
-
-            while (start < end && vowels.indexOf(word[end]) == -1) {
-                end--;
-            }
-
-
-            char temp = word[start];
-            word[start] = word[end];
-            word[end] = temp;
-
-
-            start++;
-            end--;
+    static int nCr(int n, int r)
+    {
+        // code here
+        if (r > n)
+            return 0;
+        int[] prev = new int[r + 1];
+        int[] curr = new int[r + 1];
+        prev[0] = curr[0] = 1;
+        if (r == n || r == 0) {
+            return 1;
         }
-
-        String answer = new String(word);
-        return answer;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= r; j++) {
+                curr[j] = (prev[j - 1] + prev[j]) % mod;
+            }
+            prev = curr.clone();
+        }
+        return prev[r];
     }
 }
