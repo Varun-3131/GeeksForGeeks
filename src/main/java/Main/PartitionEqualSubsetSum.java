5b7fe6f102af128ce0f889433347fb38f494b59c
Varun-3131 <srivastavvarun48@gmail.com>
2024-07-16 21:47:22 +0530

    Partition Equal Subset Sum...

---
+++ src/main/java/Main/PartitionEqualSubsetSum.java
@@ -0,0 +1,45 @@
+package Main;
+
+public class PartitionEqualSubsetSum {
+    static int equalPartition(int N, int arr[])
+    {
+        // code here
+        int sum =0;
+        for(int i=0; i<N;i++){
+            sum += arr[i];
+        }
+        if(sum%2 ==1){
+            return 0;
+        }else{
+            boolean isSubsetSum = subsetSum(N, arr,  sum/2);
+            if(isSubsetSum)
+                return 1;
+            else
+                return 0;
+        }
+    }
+
+    static boolean subsetSum(int N, int arr[],int sum){
+        boolean[][] t = new boolean[N+1][sum+1];
+        for (int i = 0; i< N+1 ; i ++){
+            for (int j=0; j< sum +1 ; j++){
+                if(i == 0)
+                    t[i][j]=false;
+                if (j== 0)
+                    t[i][j]=true;
+            }
+        }
+
+        for (int i = 1; i< N+1 ; i ++){
+            for (int j=1; j< sum +1 ; j++){
+                if (arr[i-1]<=j){
+                    t[i][j]=((t[i-1][j-arr[i-1]] )||( t[i-1][j]));
+                }
+                else
+                    t[i][j]= (t[i-1][j]);
+            }
+
+        }
+        return t[N][sum];
+    }
+}
