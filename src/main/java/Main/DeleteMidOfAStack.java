49d1beed45808ad450baa6a31a4557493b386f8e
Varun-3131 <srivastavvarun48@gmail.com>
2024-07-25 19:06:24 +0530

    Delete Mid Of A Stack...

---
+++ src/main/java/Main/DeleteMidOfAStack.java
@@ -0,0 +1,25 @@
+package Main;
+
+public class DeleteMidOfAStack {
+    // Function to delete middle element of a stack.
+    public void delete(Stack<Integer>s, int sizeOfStack, int count ){
+        if(count == Math.floor((sizeOfStack+1)/2)){
+            s.pop();
+            return;
+        }
+        else{
+            int val = s.pop();
+            delete(s , sizeOfStack , count - 1);
+            s.push(val);
+        }
+    }
+
+
+    //Function to delete middle element of a stack.
+    public void deleteMid(Stack<Integer>s,int sizeOfStack){
+        int count = sizeOfStack;
+        delete(s , sizeOfStack , count);
+    }
+
+
+}
