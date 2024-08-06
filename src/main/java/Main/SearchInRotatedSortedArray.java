b64bf3c173ba97db5d9cb72b57ba72e23f745200
Varun-3131 <srivastavvarun48@gmail.com>
2024-08-06 17:07:12 +0530

    Search In Rotated Sorted Array...

---
+++ src/main/java/Main/SearchInRotatedSortedArray.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class SearchInRotatedSortedArray {
+    int search(int[] arr, int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
