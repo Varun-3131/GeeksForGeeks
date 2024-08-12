bb9666af7e22277f8f927a25e49060f51fffdd8f
Varun-3131 <srivastavvarun48@gmail.com>
2024-08-12 15:31:03 +0530

    Insertion Sort...

---
+++ src/main/java/Main/InsertionSort.java
@@ -0,0 +1,27 @@
+package Main;
+
+public class InsertionSort {
+    static void insert(int arr[],int i)
+    {
+        // Your code here
+        for(int j=i+1; j>0; j--){
+            if(arr[j]<arr[j-1]){
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+            else {
+                break;
+            }
+        }
+
+    }
+    //Function to sort the array using insertion sort algorithm.
+    public void insertionSort(int arr[], int n)
+    {
+        //code here
+        for(int i=0; i<n-1; i++) {
+            insert(arr, i);
+        }
+    }
+}
