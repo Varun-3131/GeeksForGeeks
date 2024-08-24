a01cda8b0c661db1989e5459970629a1b6478b77
Varun-3131 <srivastavvarun48@gmail.com>
2024-08-24 23:09:27 +0530

    Convert Array Into Zig Zag Fashion...

---
+++ src/main/java/Main/ConvertArrayIntoZigZagFashion.java
@@ -0,0 +1,31 @@
+package Main;
+
+public class ConvertArrayIntoZigZagFashion {
+    public static void zigZag(int n, int[] a) {
+        // code here
+        for(int i=0;i<n-1;i++)
+        {
+            if(i%2==0)
+            {
+                if(a[i]>a[i+1])
+                {
+                    int temp=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=temp;
+
+                }
+
+            }
+            else
+            {
+                if(a[i]<a[i+1])
+                {
+                    int temp=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=temp;
+                }
+            }
+        }
+
+    }
+}
