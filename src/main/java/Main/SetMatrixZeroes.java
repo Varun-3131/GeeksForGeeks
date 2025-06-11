691f5b80df4eee1511aa295b7a0e7c649360acfa
Varun-3131 <srivastavvarun48@gmail.com>
2025-06-11 17:23:47 +0530

    Set Matrix Zeroes...

---
+++ src/main/java/Main/SetMatrixZeroes.java
@@ -0,0 +1,55 @@
+package Main;
+
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        boolean firstRow = false, firstCol = false;
+
+        //  Check if first row has zero
+        for (int j = 0; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                firstRow = true;
+                break;
+            }
+        }
+
+        //  Check if first column has zero
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                firstCol = true;
+                break;
+            }
+        }
+
+        //  Mark zeroes in first row and column
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Set zeroes based on markers
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Handle first row
+        if (firstRow) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        //Handle first column
+        if (firstCol) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
